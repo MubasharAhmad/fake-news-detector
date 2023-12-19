e838e1fde3da22d3a86e0eaca671efb245aee5bc
Safee-ullah1 <70797527+Safee-ullah1@users.noreply.github.com>
2023-12-19 11:13:41 +0500

    Fixed inference error

--- src/app/api/inference/route.tsx
+++ src/app/api/inference/route.tsx
@@ -4,10 +4,9 @@ import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HumanMessage, SystemMessage } from "langchain/schema";
 
 export async function POST(req: NextRequest) {
-    console.log(env.OPENAI_API_KEY);
 	const model = new ChatOpenAI({
-        openAIApiKey: "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",
-		temperature: 0.9,
+        openAIApiKey: env.OPENAI_API_KEY,
+		temperature: 0,
 		modelName: "ft:gpt-3.5-turbo-1106:ir-project-org::8XMK0Zpc",
 	});
 	try {
--- src/app/detect-news/page.tsx
+++ src/app/detect-news/page.tsx
@@ -1,41 +1,65 @@
-"use client"
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Textarea } from "@/components/ui/textarea";
+import axios from "axios";
 
 const DetectNews = () => {
+	const [news, setNewsTitle] = useState("");
+	const [isTrue, setIsTrue] = useState(false);
+	const [result, setResult] = useState("");
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-  }
-  return (
-    <div className="container mt-28 overflow-hidden">
-      <Card className="mx-2 sm:mx-auto">
-        <CardHeader>
-          <CardTitle>Detect News</CardTitle>
-          <CardDescription>Enter your news below to detect if its false or true.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={handleSubmit}>
-            <div className="grid w-full items-center gap-4">
-              <Textarea placeholder="Type your news here..." />
-              <div className='flex justify-end'>
-                <Button type='submit' className='w-32'>Detect</Button>
-              </div>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
-  )
-}
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		// check if info is a url
+    e.preventDefault();
+		console.log(news);
+		const res = await axios.post("/api/inference", {
+			news,
+		});
+		console.log(res.data);
+		const { result } = res.data;
+		setResult(result);
+		if (result.includes("true")) {
+			setIsTrue(true);
+		}
+	};
+	return (
+		<div className='container mt-28 overflow-hidden'>
+			<Card className='mx-2 sm:mx-auto'>
+				<CardHeader>
+					<CardTitle>Detect News</CardTitle>
+					<CardDescription>Enter your news below to detect if its false or true.</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<form onSubmit={handleSubmit}>
+						<div className='grid w-full items-center gap-4'>
+							<Textarea placeholder='Type your news here...' onChange={(e)=>{
+                setNewsTitle(e.currentTarget.value);
+              }} />
+							<div className='flex justify-end'>
+								<Button
+									type='submit'
+									className='w-32'
+								>
+									Detect
+								</Button>
+							</div>
+						</div>
+					</form>
+					<div>
+						{result ??
+							(isTrue ? (
+								<h1 className='text-green-500'>{result}</h1>
+							) : (
+								<h1 className='text-red-500'>{result}</h1>
+							))}
+					</div>
+				</CardContent>
+			</Card>
+		</div>
+	);
+};
 
-export default DetectNews
+export default DetectNews;
